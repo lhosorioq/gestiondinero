277f688f3da23aec4cdd6583d860bbd15d88b2f6
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2021-10-12 16:20:39 -0500

    Pagina totals

---
+++ client/src/views/Totals.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <v-container>
+
+        <barra-navegacion/>
+
+        <totals-comp/>
+
+        <Footer/>
+
+    </v-container>
+</template>
+
+<script>
+import BarraNavegacion from '../components/BarraNavegacion.vue'
+import Footer from '../components/Footer.vue'
+import TotalsComp from '../components/TotalsComp.vue'
+
+export default {
+    components: { BarraNavegacion, Footer, TotalsComp },
+
+}
+</script>
+
+<style>
+
+</style>
